cc092538cd5f2c56ca5b3df8267a389e5425bd12
Phil Parisi <philsbeginnercode@gmail.com>
2022-04-20 11:52:54 -0400

    added better plotting methods for homomorphic.m

--- homomorphic.m
+++ homomorphic.m
@@ -33,15 +33,15 @@ toc
 clc, close all, format compact, clearvars -except ex1_raw
 
 % Threshold Raw Image
-ex1_thresh = 0.20;
-ex1_thresh = imbinarize(ex1_raw, ex1_thresh);
+thresh4ex1 = 0.20;
+ex1_thresh= imbinarize(ex1_raw, thresh4ex1);
 
 % Homomorphic Filter & Side-by-Side Comparisons
 I = ex1_raw;            % create local test copy
 image_array = {};       % define image array to hold homomorphic images
 
 % Loop over different sigma filter values and save homomorphed images
-sig = [0.5, 1, 1.5, 2, 2.5, 3, 4, 5, 10, 20, 35, 50];
+sig = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]; %, 4, 5, 10, 20, 35, 50];
 
 for i = 1:length(sig)    
 
@@ -50,16 +50,16 @@ for i = 1:length(sig)
 
     % Thresholding
     img = image_array{i};
-    thresh_val = graythresh(img);
-    img_thresh = imbinarize(img,thresh_val);
+    thresh4img = graythresh(img);
+    img_thresh = imbinarize(img,thresh4img);
 
     figure
-    subplot(2,1,1)
-    imshowpair(ex1_thresh, img_thresh, 'montage')
-    title('Raw Image w/ Thresh (left) vs. Homomorphed w/ Thresh (right)')
-    subplot(2,1,2)
+    subplot(2,1,1) % Raw and Homomorphic Image
     imshowpair(ex1_raw, img, 'montage')
-    title(strcat("Raw Image (left) vs. Homomorphed w/ ",num2str(sig(i))," (right)"))
+    title(strcat("Raw Image (left) vs. Homomorphed w/ Sigma = ",num2str(sig(i))," (right)"))
+    subplot(2,1,2) % Raw Thresholded and Homo Thresholded w/ Graythresh
+    imshowpair(ex1_thresh, img_thresh, 'montage')
+    title(strcat("Raw Image w/ ",num2str(thresh4ex1)," Thresh (left) vs. Homomorphed w/ ",num2str(round(thresh4img,2))," Thresh (right)"))
     pause(1)
 
 end
